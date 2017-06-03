bdedc5edd3902098ff126a1d6d09135bacec48d8
Tatsuki <tatsuki@konokaze.jp>
2017-06-04 00:57:43 +0900

    とりあえずREPLまで

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
 import System.Environment
+import Control.Monad
 import MyParser
 
 main :: IO ()
 main = do
         args <- getArgs
-        putStrLn (readExpr (args !! 0))
+        case length args of
+            0         -> runRepl
+            1         -> evalAndPrint $ args !! 0
+            otherwise -> putStrLn "Program takes only 0 or 1 argment"
